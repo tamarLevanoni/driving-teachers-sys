f32234c31c7cd0477fe5733ee0528adc095dbf93
ReutAttar <32901809+ReutAttar@users.noreply.github.com>
2021-03-09 18:54:23 +0200

    add router

--- src/App.js
+++ src/App.js
@@ -1,9 +1,42 @@
 import "./App.css";
 import HomePage from "./components/HomePage/HomePage";
 import Students from "./components/Students/Students";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { useEffect, useState } from "react";
 
 const App = () => {
-  return <HomePage />;
+  const [students, setStudents] = useState([]);
+
+  useEffect(() => {
+    async function fetchData() {
+      const res = await fetch("http://localhost:8000/products"); //"https://fakestoreapi.com/products");
+      const json = await res.json();
+      setStudents(json);
+    }
+
+    fetchData();
+  }, []);
+  return (
+    <Router>
+      {/* <nav className="navigation">
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/about">About</Link>
+        </li>
+      </ul>
+    </nav> */}
+
+      <Switch>
+        <Route path="/students" component={Students} />
+        <Route path="/">
+          <HomePage />
+        </Route>
+      </Switch>
+    </Router>
+  );
 };
 
 export default App;
